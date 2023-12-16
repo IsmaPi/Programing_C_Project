dea7a5168307d1c2d161924aa9245c3e5323f7db
Luis Gomez-Acebo <124214330+Fruitkeeper@users.noreply.github.com>
2023-12-16 11:19:06 +0100

    Adding tokens in lexer.h

--- lexer.h
+++ lexer.h
@@ -11,6 +11,22 @@ typedef enum {
     TOKEN_LPAREN,
     TOKEN_RPAREN,
     TOKEN_VARIABLE, // General variable token type
+    TOKEN_COS, 
+    TOKEN_SIN,
+    TOKEN_PI,
+    TOKEN_E, 
+    TOKEN_SQRT, 
+    TOKEN_LOG, 
+    TOKEN_EXP, 
+    TOKEN_ABS, 
+    TOKEN_TAN, 
+    TOKEN_ASIN, 
+    TOKEN_ACOS, 
+    TOKEN_ATAN, 
+    TOKEN_DERIVATIVE, 
+    TOKEN_INTEGRAL,
+    TOKEN_POWER, 
+    TOKEN_MODULO, 
     TOKEN_EOF,
 } TokenType;
 
